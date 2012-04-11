ca1c5b8c79b90647ea95412f9d81ac930614014b
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2012-04-11 23:16:38 +0000

    remove junk

--- src/NSAppleEventDescriptor+TDAdditions.m
+++ src/NSAppleEventDescriptor+TDAdditions.m
@@ -65,8 +65,6 @@
 
 
 - (OSErr)sendToOwnProcessWaitReply:(AppleEvent *)replyEvt {
-    //[self setDescriptor:[NSAppleEventDescriptor descriptorWithString:@"foobar"] forKeyword:'iTod'];
-    
     const AppleEvent *aevt = [self aeDesc];
     
     OSErr err = noErr; 
