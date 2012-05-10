24ee32ba21ed7412df86af310607547e14f53658
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2012-05-10 02:30:15 +0000

    tag-shapes-2.3-2.4

---
+++ tags/tag-shapes-2.3-2.4/src/TDViewController.m
@@ -0,0 +1,84 @@
+//
+//  TDViewController.m
+//  TDAppKit
+//
+//  Created by Todd Ditchendorf on 11/10/10.
+//  Copyright 2010 Todd Ditchendorf. All rights reserved.
+//
+
+#import <TDAppKit/TDViewController.h>
+#import <TDAppKit/TDView.h>
+
+@implementation TDViewController
+
+- (void)dealloc {
+#ifdef TDDEBUG
+    NSLog(@"%s %@", __PRETTY_FUNCTION__, self);
+#endif
+    NSView *v = [self view];
+    if (v) {
+        [(TDView *)v setViewController:nil];
+    }
+    [super dealloc];
+}
+
+
+- (void)loadView {
+    [super loadView];    
+    [self viewDidLoad];
+}
+
+
+- (void)setView:(NSView *)v {
+    if (v) {
+        [(TDView *)v setViewController:self];
+    }
+    [super setView:v];
+}
+
+
+- (void)viewDidLoad {
+    
+}
+
+
+- (void)viewWillAppear {
+    
+}
+
+
+- (void)viewDidAppear {
+    
+}
+
+
+- (void)viewWillDisappear {
+    
+}
+
+
+- (void)viewDidDisappear {
+    
+}
+
+
+- (void)viewWillMoveToSuperview:(NSView *)v {
+    
+}
+
+
+- (void)viewDidMoveToSuperview {
+    
+}
+
+
+- (void)viewWillMoveToWindow:(NSWindow *)win {
+    
+}
+
+
+- (void)viewDidMoveToWindow {
+    
+}
+
+@end
