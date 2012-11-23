db984cdaf353cae95539297361ffe735e6a3ae07
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2012-11-23 21:24:21 +0000

    Statusbar popup view and button

---
+++ src/TDStatusBarButton.m
@@ -0,0 +1,17 @@
+//
+//  TDStatusBarButton.m
+//  TDAppKit
+//
+//  Created by Todd Ditchendorf on 11/23/12.
+//  Copyright (c) 2012 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDStatusBarButton.h"
+
+@implementation TDStatusBarButton
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+}
+
+@end
