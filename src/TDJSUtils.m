b86b662849e47c865cfe8a4bb6ec38eefa3a0ec6
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2013-05-08 02:08:11 +0000

    import

--- src/TDJSUtils.m
+++ src/TDJSUtils.m
@@ -12,7 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import "TDJSUtils.h"
+#import <TDAppKit/TDJSUtils.h>
 
 NSString *TDJSStringGetNSString(JSStringRef str) {
     return [(id)JSStringCopyCFString(NULL, str) autorelease];
