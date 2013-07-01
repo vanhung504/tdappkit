e762e8eec1183496843d07065d58292f3e459aa7
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2013-07-01 21:11:26 +0000

    status bar button

---
+++ trunk/include/TDAppKit/TDStatusBarButton.h
@@ -0,0 +1,15 @@
+//
+//  TDStatusBarButton.h
+//  TDAppKit
+//
+//  Created by Todd Ditchendorf on 7/1/13.
+//  Copyright (c) 2013 Todd Ditchendorf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TDStatusBarButton : NSButton
+
+- (NSRect)titleRectForBounds:(NSRect)bounds;
+
+@end
