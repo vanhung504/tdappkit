d6da4244e3b96b03781931863a8278bc3fad7054
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2013-08-05 22:03:04 +0000

    TDTabBar

--- include/TDAppKit/TDTabBar.h
+++ include/TDAppKit/TDTabBar.h
@@ -12,9 +12,9 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#import <TDAppKit/TDFlippedView.h>
+#import <TDAppKit/TDBar.h>
 
-@interface TDTabBar : TDFlippedView
+@interface TDTabBar : TDBar
 
 + (CGFloat)defaultHeight;
 @end
