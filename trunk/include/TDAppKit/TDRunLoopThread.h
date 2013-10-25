db11929c64e7be18e0314b9449dd41a9b59209c6
todd.ditchendorf <todd.ditchendorf@920da3f4-33dd-3378-2bda-26f8a5ac8392>
2013-10-25 19:51:00 +0000

    TDRunLoopThread

---
+++ trunk/include/TDAppKit/TDRunLoopThread.h
@@ -0,0 +1,23 @@
+//
+//  TDRunLoopThread.h
+//  TDAppKit
+//
+//  Created by Todd Ditchendorf on 10/25/13.
+//  Copyright (c) 2013 Todd Ditchendorf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^TDBlock)(void);
+typedef id (^TDRunBlock)(NSError **outErr);
+typedef void (^TDCompletionBlock)(id result, NSError *err);
+
+@interface TDRunLoopThread : NSObject
+
+- (void)start;
+
+- (void)runOnThread:(TDRunBlock)block completion:(TDCompletionBlock)completion;
+
+- (void)executeOnThread:(TDBlock)block; // wait=NO
+- (void)executeOnThread:(TDBlock)block waitUntilDone:(BOOL)wait;
+@end
